3fc3a06a3ed8c2e67d0d24d05cede09472827509
Hiroshi Sugawara <5000164@users.noreply.github.com>
2020-02-26 00:14:32 +0100

    feat: adding a component for Static pages

---
+++ src/components/Static/index.js
@@ -0,0 +1,32 @@
+import React from "react"
+import styled from "styled-components"
+
+const Static = ({ title, content }) => {
+  return (
+    <>
+      <StyledTitle>{title}</StyledTitle>
+      <StyledContent dangerouslySetInnerHTML={{ __html: content }} />
+    </>
+  )
+}
+
+const StyledTitle = styled.div`
+  width: 600px;
+  margin: 20px auto;
+  font-size: 2rem;
+  text-align: center;
+  color: hsl(235, 10%, 5%);
+  @media (max-width: 800px) {
+    width: 95%;
+  }
+`
+
+const StyledContent = styled.article`
+  width: 600px;
+  margin: 20px auto;
+  @media (max-width: 800px) {
+    width: 75%;
+  }
+`
+
+export default Static
