92339a95af210eb7307f7cf713ac7f879ecca999
Hiroshi Sugawara <5000164@users.noreply.github.com>
2020-02-26 00:15:18 +0100

    chore: to export to use components a blog site which is using this theme

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// noop
+export { default as Layout } from "./src/components/Layout"
+export { default as SEO } from "./src/components/SEO"
+export { default as Static } from "./src/components/Static"
