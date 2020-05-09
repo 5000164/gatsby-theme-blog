1a96b1eeb7e281d743b89c740a6d1830aed474e9
Hiroshi Sugawara <5000164@users.noreply.github.com>
2020-05-09 18:49:10 +0200

    chore: formatting

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -1,5 +1,5 @@
-const trackingId = "UA-51024599-1";
-const anonymize = true;
+const trackingId = "UA-51024599-1"
+const anonymize = true
 
 module.exports = {
   siteMetadata: {
@@ -9,10 +9,9 @@ module.exports = {
     profile:
       "Web プログラマー。好きな言葉は「安定的に不安定」。オーストラリアで英語を勉強した後に、現在デンマークで幸せについて考えている。",
     consent: {
-      text:
-        "このウェブサイトでは Cookie を使用します。<a href='/privacy/'>詳細はこちら</a>",
+      text: "このウェブサイトでは Cookie を使用します。<a href='/privacy/'>詳細はこちら</a>",
       accept: "同意する",
-      decline: "同意しない"
+      decline: "同意しない",
     },
     siteUrl: `https://blog.5000164.jp`,
     image: `/icon.png`,
@@ -20,26 +19,26 @@ module.exports = {
     repository: `https://github.com/5000164/blog.5000164.jp`,
     profileUrl: `https://5000164.jp`,
     trackingId,
-    anonymize
+    anonymize,
   },
   plugins: [
     {
       resolve: "theme",
-      options: {}
+      options: {},
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `articles`,
-        path: `${__dirname}/src/posts`
-      }
+        path: `${__dirname}/src/posts`,
+      },
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -50,14 +49,14 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#ffffff`,
         display: `minimal-ui`,
-        icon: `src/images/icon.png`
-      }
+        icon: `src/images/icon.png`,
+      },
     },
     {
       resolve: `gatsby-plugin-canonical-urls`,
       options: {
-        siteUrl: `https://blog.5000164.jp`
-      }
+        siteUrl: `https://blog.5000164.jp`,
+      },
     },
     {
       resolve: `gatsby-plugin-gdpr-cookies`,
@@ -65,14 +64,14 @@ module.exports = {
         googleAnalytics: {
           trackingId,
           cookieName: "gatsby-gdpr-google-analytics",
-          anonymize
+          anonymize,
         },
         facebookPixel: {
           pixelId: "",
-          cookieName: "gatsby-gdpr-facebook-pixel"
+          cookieName: "gatsby-gdpr-facebook-pixel",
         },
-        environments: ["production", "development"]
-      }
-    }
-  ]
-};
+        environments: ["production", "development"],
+      },
+    },
+  ],
+}
