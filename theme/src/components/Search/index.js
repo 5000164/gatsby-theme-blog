7586ca61819394e76263daca35fee5b22b3a5d37
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-01-14 16:36:55 +0900

    chore: Update libraries

--- theme/src/components/Search/index.js
+++ theme/src/components/Search/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import algoliasearch from "algoliasearch/lite"
 import { connectStateResults, Hits, Index, InstantSearch, SearchBox } from "react-instantsearch-dom"
 import styled, { css } from "styled-components"
-import { Algolia } from "styled-icons/fa-brands/Algolia"
+import { Algolia } from "@styled-icons/fa-brands/Algolia"
 import PostHit from "./PostHit"
 
 export default class Search extends Component {
