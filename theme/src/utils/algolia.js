0af18394bbc1c10ad15a64722cb62ebdcd443ead
Hiroshi Sugawara <5000164@users.noreply.github.com>
2021-03-08 13:54:55 +0900

    build: Build without .env

--- theme/src/utils/algolia.js
+++ theme/src/utils/algolia.js
@@ -1,9 +1,4 @@
-const currentEnvironment = process.env.ENV || process.env.NODE_ENV || "development"
-const queries =
-  currentEnvironment !== "algolia"
-    ? []
-    : (() => {
-        const postQuery = `{
+const postQuery = `{
   posts: allMarkdownRemark(
     filter: { fileAbsolutePath: { regex: "/posts/" } }
   ) {
@@ -24,22 +19,21 @@ const queries =
   }
 }`
 
-        const flatten = arr =>
-          arr.map(({ node: { fields, frontmatter, ...rest } }) => ({
-            ...fields,
-            ...frontmatter,
-            ...rest,
-          }))
-        const settings = { attributesToSnippet: [`excerpt:20`] }
+const flatten = (arr) =>
+  arr.map(({ node: { fields, frontmatter, ...rest } }) => ({
+    ...fields,
+    ...frontmatter,
+    ...rest,
+  }))
+const settings = { attributesToSnippet: [`excerpt:20`] }
 
-        return [
-          {
-            query: postQuery,
-            transformer: ({ data }) => flatten(data.posts.edges),
-            indexName: `Posts`,
-            settings,
-          },
-        ]
-      })()
+const queries = [
+  {
+    query: postQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: `Posts`,
+    settings,
+  },
+]
 
 module.exports = queries
